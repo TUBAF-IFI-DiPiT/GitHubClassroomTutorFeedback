ce67f99c6acb0075b5abe1b1b216a994d665013f
SebastianZug <sebastian.zug@informatik.tu-freiberg.de>
2021-06-17 15:18:20 +0200

    Dont talk about!

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ pipenv run python -m ipykernel install --user --name mygreatenv --display-name "
 pipenv run python ./src/Aggregation.py   
 cd notebooks
 #pipenv run jupyter nbconvert --to notebook --inplace --execute 00_Aggregation.ipynb
-pipenv jupyter nbconvert --to notebook --inplace --execute 01_Preprocessing.ipynb
-pipenv jupyter nbconvert --to notebook --inplace --execute 02_FeedbackGeneration.ipynb
-pipenv jupyter nbconvert --to markdown --TemplateExporter.exclude_input=True 02_FeedbackGeneration.ipynb
+pipenv run jupyter nbconvert --to notebook --inplace --execute 01_Preprocessing.ipynb
+pipenv run jupyter nbconvert --to notebook --inplace --execute 02_FeedbackGeneration.ipynb
+pipenv run jupyter nbconvert --to markdown --TemplateExporter.exclude_input=True 02_FeedbackGeneration.ipynb
 sed "s/02_FeedbackGeneration_files/notebooks\/02_FeedbackGeneration_file/g" 02_FeedbackGeneration.md > output.md
 sed 's/^[ \t]*//;s/[ \t]*$//' output.md > Readme.md
 mv Readme.md ../README.md
