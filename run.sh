47139af38cdcf25f23d89996dd4b6da1a2fa4904
SebastianZug <sebastian.zug@informatik.tu-freiberg.de>
2021-06-17 07:03:35 +0200

    Adapt bash script structure

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-cd notebooks
 pipenv run python -m ipykernel install --user --name mygreatenv --display-name "My Great Env"
-#pipenv run jupyter nbconvert --to notebook --inplace --execute 00_Aggregation.ipynb
 pipenv run python ./src/Aggregation.py   
+cd notebooks
+#pipenv run jupyter nbconvert --to notebook --inplace --execute 00_Aggregation.ipynb
 jupyter nbconvert --to notebook --inplace --execute 01_Preprocessing.ipynb
 jupyter nbconvert --to notebook --inplace --execute 02_FeedbackGeneration.ipynb
 jupyter nbconvert --to markdown --TemplateExporter.exclude_input=True 02_FeedbackGeneration.ipynb
