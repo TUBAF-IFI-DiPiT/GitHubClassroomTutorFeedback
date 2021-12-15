2a29ef8a3f5dfd31c026bd6d2037e9ba2da7d442
SebastianZug <sebastian.zug@informatik.tu-freiberg.de>
2021-12-15 09:48:24 +0100

    Integrate github2pandas_manager call

--- FeedbackGeneration/__main__.py
+++ FeedbackGeneration/__main__.py
@@ -9,7 +9,35 @@ from github2pandas_manager import utilities
 sys.path.append(os.path.join(sys.path[0],'FeedbackGeneration'))
 from FeedbackGenerator import FeedbackGenerator
 
-def main(request_params):
+from github2pandas_manager.config_parser import YAML_RequestDefinition
+from github2pandas_manager.repository_handler import RequestHandlerFactory
+from github2pandas_manager.data_extractor import Github_data_extractor
+from github2pandas_manager.data_merger import Github_data_merger
+from github2pandas_manager import utilities
+
+def call_github2pandas_manager(request_params, github_token):
+    project_folder = Path(request_params.parameters.project_folder)
+    project_folder.mkdir(parents=True, exist_ok=True)
+
+    request_handler = \
+        RequestHandlerFactory.get_request_handler(
+                github_token=github_token,
+                request_params=request_params
+            )
+
+    print(f"{len(request_handler.repository_list)} machting repositories found.")
+
+    if len(request_handler.repository_list) > 0:
+        data_extractor = Github_data_extractor.start(
+                github_token=github_token,
+                request_handler=request_handler
+        )
+        
+        df = Github_data_merger.merge(
+            request_handler=request_handler
+            )
+
+def call_feedback_generation(request_params):
     project_folder = Path(request_params.parameters.project_folder)
     if not project_folder:
         print("Project folder and data set does not exist!")
@@ -28,4 +56,9 @@ if __name__ == "__main__":
 
     arguments = parser.parse_args()
     request_params = YAML_RequestDefinition(arguments.config_file)
-    main(request_params=request_params)
+    
+    github_token = os.environ['TOKEN']
+    call_github2pandas_manager(request_params=request_params, 
+                               github_token=github_token)
+    
+    call_feedback_generation(request_params=request_params)
